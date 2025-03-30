f54492103b7e113151ede9b347bc797758a97c6d
Raees Kabir <kabir.raees@gmail.com>
2025-03-29 23:33:00 -0400

    amend style error

--- api_services/itunes.py
+++ api_services/itunes.py
@@ -1,6 +1,7 @@
 """iTunes API"""
 import requests
 
+
 def get_track_summary(artist: str, title: str) -> dict:
     """Returns a dictionary with song details based on title and artist search.
     Returns an empty dictionary if there is an error finding the song."""
